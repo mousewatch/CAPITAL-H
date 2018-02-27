793fec0312c4442fdbca3c2ed8219e5c276ee1c4
apolocervntes <cecgclaudioe@gmail.com>
2018-02-26 18:32:44 -0600

    Add files via upload

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
-﻿<?php 
+﻿<?php
 $errors = '';
-$myemail ='capitalhcontacto@gmail.com';
+$myemail = 'capitalhcontacto@gmail.com';
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['message']))
@@ -13,7 +13,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 
 if (!eregi(
-"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", 
+"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$",
 $email))
 {
 $errors .= "\n Error: Invalid Email Address";
